74ac7c14a10438686a6f49aee455ce07f4ba0f11
Kikis <jaitovichbidnaud@gmail.com>
2026-05-20 08:33:27 -0600

    Fix: eliminar nomnre extra en el footer

--- chagasSam/src/components/Footer.jsx
+++ chagasSam/src/components/Footer.jsx
@@ -18,7 +18,6 @@ export default function Footer() {
         <p className="text-sm mt-4 text-gray-400"> Mendoza Salazar Larissa </p>
         <p className="text-sm mt-1 text-gray-400"> Ozuna Toledo Valentina </p>
         <p className="text-sm mt-1 text-gray-400"> Salazar Ángel Monserrat </p>
-        <p className="text-sm mt-1 text-gray-400"> Mendoza Salazar Larissa </p>
         <p className="text-sm mt-1 text-gray-400"> Velázquez Magdaleno Luna Valeria </p>
         <p className="text-sm mt-4 text-gray-500">&copy; {year}</p>
       </div>
